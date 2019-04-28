f88b5eca879ecb8ebd38a24a89574aaf1aa36348
ziratsu <enzo.ustariz@hotmail.fr>
2019-04-28 18:52:57 +0200

    Updates

---
+++ precache-manifest.4ba0f82062b0a20f688a9a4c35a447b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc15f56e16b2237a9c838ebb3b4a501",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "b6dd522b358473170250",
+    "url": "/todolistreact/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b6dd522b358473170250",
+    "url": "/todolistreact/static/js/2.b7fa8f2a.chunk.js"
+  },
+  {
+    "revision": "ce106e01f981c0a5618a",
+    "url": "/todolistreact/static/js/main.3d6ce1fc.chunk.js"
+  },
+  {
+    "revision": "aaa5b9d8f066e13337eb",
+    "url": "/todolistreact/static/js/runtime~main.9bd22045.js"
+  }
+]);
